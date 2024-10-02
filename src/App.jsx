2108bb9794029da9f5def498daeddb80aca44812
Siddhesh Waghmare <160401157+SiddheshWaghmare@users.noreply.github.com>
2024-10-02 22:10:33 +0530

    Updated App.jsx( Homepage imported)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard'; // Update the path to point to the components folder
-import DashboardHistory from './components/DashboardHistory'; // Update the path to point to the components folder
+import EnhancedHomepage from './components/EnhancedHomepage';
+import Dashboard from './components/Dashboard';
+import DashboardHistory from './components/DashboardHistory';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<EnhancedHomepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<DashboardHistory />} />
       </Routes>
     </Router>
