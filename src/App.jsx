9ba107d3aeb679422462736bc3dddc7f487adb0f
Yadavatharva2003 <yadavatharva2003@gmail.com>
2024-10-04 12:37:03 +0530

    Revert "Update App.jsx"
    This reverts commit d3993197277a9ef9b4313628c95fff46ed70c659.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import Dashboard from './components/Dashboard';
 import DashboardHistory from './components/DashboardHistory';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-import AboutUs from './components/AboutUs'; // Add this import
-import AccountSettings from './components/AccountSettings'; // Add this import
+
 
 const App = () => {
   return (
@@ -17,8 +16,6 @@ const App = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<DashboardHistory />} />
-        <Route path="/about" element={<AboutUs />} /> // Add this new route
-        <Route path="/account-settings" element={<AccountSettings />} /> // Add this new route
       </Routes>
     </Router>
   );
